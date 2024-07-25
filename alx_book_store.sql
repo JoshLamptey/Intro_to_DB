b7754524d6dc1baf01236fd85a2e11fa18994866
JoshLamptey <joshlamptey75@gmail.com>
2024-07-25 04:24:04 -1200

    this is hard and i need to start attending lectures

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,29 +5,33 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books(
     book_id INT AUTO_INCREMENT Primary Key ,
     title VARCHAR(130) NOT NULL,
-    author_id FOREIGN KEY REFERENCES Authors(author_id) NOT NULL,
+    author_id INT,
     price DOUBLE NOT NULL,
-    publication_date DATE
-)
+    publication_date DATE,
+     FOREIGN KEY (author_id) REFERENCES Authors(author_id) 
+);
 
 CREATE TABLE Authors(
     author_id INT AUTO_INCREMENT Primary Key ,
-    author_name VARCHAR(215)
-)
+    author_name VARCHAR(215) NOT NULL
+);
 CREATE TABLE Customers(
-    customer_id(Primary Key) NOT NULL,
+    customer_id INT AUTO_INCREMENT Primary Key,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
-)
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT 
+);
 CREATE TABLE Orders(
-    order_id(Primary Key) NOT NULL,
-    customer_id FOREIGN KEY REFERENCES Customers(customer_id) NOT NULL,
-    order_date DATE
+    order_id INT AUTO_INCREMENT Primary Key NOT NULL,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 CREATE TABLE Order_Details(
-    orderdetailid (Primary Key) NOT NULL,
-    order_id FOREIGN KEY REFERENCES Orders(order_id) NOT NULL,
-    book_id FOREIGN KEY REFERENCES Books(book_id) NOT NULL,
-    quantity DOUBLE PRECISON
+    orderdetailid INT AUTO_INCREMENT Primary Key,
+    order_id INT ,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
